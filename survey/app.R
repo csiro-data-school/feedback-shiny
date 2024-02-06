7d32c003f887eb8f0fc57d860f6d66cfa6192aee
Alex Whan <alexwhan@gmail.com>
2024-02-06 16:22:01 +1100

    initial commit. working survey app

---
+++ survey/app.R
@@ -0,0 +1,37 @@
+library(shinysurveys)
+
+ui <- shiny::fluidPage(
+  uiOutput("paceInput"),
+  actionButton("submit", "Submit")
+  )
+
+
+server <- function(input, output, session) {
+  
+  outputPath <- "../data/responses.csv"
+  formData <- reactive({
+    data <- data.frame(timestamp = Sys.time(),
+                       response = input$paceInput)
+  })
+  
+  saveData <- function(data) {
+    write.table(formData(), outputPath, row.names = FALSE,
+                append = TRUE, sep = ",", col.names = FALSE)
+  }
+ 
+  output$paceInput <- renderUI({
+    input$submit
+    tagList(
+      radioButtons("paceInput", 
+                   "How is the teaching pace right now?",
+                   c("Too slow", "Just right", "Too fast"),
+                   selected = character(0))
+    )
+  })
+  
+  observeEvent(input$submit, {
+    saveData()
+  })
+}
+
+shiny::shinyApp(ui = ui, server = server)
